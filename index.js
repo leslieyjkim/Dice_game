2708a6cb892146828e1d1b0068ad471a29d620e2
yoonjookim <yjblossom@gmail.com>
2024-08-05 16:58:06 -0400

    Added <script> tag in the html file to link index.js file

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ var imageName = "./images/dice" + randomNumber1 + ".png"
 document.getElementById("diceImage1").setAttribute("src", "imageName");
 
 // console.log(randomNumber1); //test: node index.js on Terminal
+
+
+// alert("Working!");  
+//ABOVE LINE: To check that index.js was linked to the dicee.html well.
