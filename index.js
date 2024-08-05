ee0ed746e814b5db5be654bf6b6f634a2f0f00a4
yoonjookim <yjblossom@gmail.com>
2024-08-05 17:36:23 -0400

    Separate random_case for each player1/2, 3 cases if-statement

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+//left die for player1
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var imageName1 = "./images/dice" + randomNumber1 + ".png"
 
-var imageName = "./images/dice" + randomNumber1 + ".png"
+document.getElementById("diceImage1").setAttribute("src", imageName1);
+
+//right die for player2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var imageName2 = "./images/dice" + randomNumber2 + ".png"
 
-document.getElementById("diceImage1").setAttribute("src", imageName);
-
 document.getElementById("diceImage2").setAttribute("src", imageName2);
+
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = " Player 2 Wins!  🚩";
+} else {
+    document.querySelector("h1").innerHTML = "🟰 Draw! 🟰"
+}
+
+
 
 // console.log(randomNumber1); //test: node index.js on Terminal
 
