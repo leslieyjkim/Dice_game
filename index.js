b5233f1cbe000da50a7930b49c847c422c33b7b8
yoonjookim <yjblossom@gmail.com>
2024-08-05 17:10:11 -0400

    Added randomNumber setAttribute separately to dice 1 & 2

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var imageName = "./images/dice" + randomNumber1 + ".png"
+var imageName2 = "./images/dice" + randomNumber2 + ".png"
 
-document.getElementById("diceImage1").setAttribute("src", "imageName");
+document.getElementById("diceImage1").setAttribute("src", imageName);
+
+document.getElementById("diceImage2").setAttribute("src", imageName2);
 
 // console.log(randomNumber1); //test: node index.js on Terminal
 
